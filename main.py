263bd6fb378c3f62cda897822a296e3fdfd60125
Komiljon <143028047+Komil-jon@users.noreply.github.com>
2024-07-06 13:19:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ class FaceRecognition:
         self.encode_faces()
 
     def encode_faces(self):
-        for image in os.listdir('faces'):
-            face_image = face_recognition.load_image_file(f'faces/{image}')
+        for image in os.listdir('photos'):
+            face_image = face_recognition.load_image_file(f'photos/{image}')
             face_encoding = face_recognition.face_encodings(face_image)[0]
 
             self.known_face_encodings.append(face_encoding)
